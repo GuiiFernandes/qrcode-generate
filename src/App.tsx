cef39ca60f4092faf9a13846fdd5ce3439d13887
Guilherme Fernandes <guifjj92@gmail.com>
2023-11-21 09:44:36 -0300

    feat: Criei aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
+import { useQRCode } from 'next-qrcode';
+import { useState } from 'react';
+
+import { Main, Form, Input } from './styled';
+
 function App() {
+  const { Canvas } = useQRCode();
+  const [url, setUrl] = useState('');
+
   return (
-    <main>GERADOR DE QRCODE</main>
+    <Main>
+      <h1>GERADOR DE QRCODE</h1>
+      <Form>
+        <Input
+          type="url"
+          id="url"
+          value={ url }
+          placeholder="insira sua url"
+          onChange={ ({ target }) => setUrl(target.value) }
+        />
+        { url && (
+          <Canvas
+            text={ url }
+            options={ {
+              errorCorrectionLevel: 'M',
+              margin: 3,
+              scale: 4,
+              width: 200,
+              color: {
+                dark: '#000',
+                light: '#fff',
+              },
+            } }
+          />
+        ) }
+      </Form>
+    </Main>
   );
 }
 
